57e324b88de08eb6c4bafeb9d41960b120191bc1
Sergey Ratiashvili <serg.s.r@gmail.com>
2023-05-29 22:16:57 +0200

    feat: fake-vault

---
+++ contracts/fake-vault/src/msg.rs
@@ -0,0 +1,33 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Dao {},
+    Description {},
+    VotingPowerAtHeight {
+        height: Option<u64>,
+        address: String,
+    },
+    TotalPowerAtHeight {
+        height: Option<u64>,
+    },
+    Info {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    SetPower { address: String, power: u64 },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub dao: String,
+    pub description: String,
+    pub info: String,
+}
---
+++ contracts/fake-vault/src/store.rs
@@ -0,0 +1,15 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cw_storage_plus::{Item, Map};
+
+pub const USERS: Map<String, u64> = Map::new("users");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Config {
+    pub dao: String,
+    pub description: String,
+    pub info: String,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");

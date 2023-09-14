57d1df34c431ddeb93e19592cbd6a80ae9ff9812
quasisamurai <quazisamurai@gmail.com>
2023-09-14 17:17:56 +0400

    fmt

--- contracts/before-send-hook/src/contract.rs
+++ contracts/before-send-hook/src/contract.rs
@@ -41,12 +41,12 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
 #[entry_point]
 pub fn sudo(deps: DepsMut, _env: Env, msg: SudoMsg) -> StdResult<Response> {
     match msg {
-        SudoMsg::TrackBeforeSendSudoMsg {
-            track_before_send,
-        } => sudo_result_track_before(deps, track_before_send),
-        SudoMsg::BlockBeforeSendSudoMsg {
-            block_before_send,
-        } => sudo_result_block_before(deps, block_before_send),
+        SudoMsg::TrackBeforeSendSudoMsg { track_before_send } => {
+            sudo_result_track_before(deps, track_before_send)
+        }
+        SudoMsg::BlockBeforeSendSudoMsg { block_before_send } => {
+            sudo_result_block_before(deps, block_before_send)
+        }
     }
 }
 

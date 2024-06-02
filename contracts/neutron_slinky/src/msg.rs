cac3d7b25b7f3f7adb433fd1f558c625d0eb9ae7
udit-gulati <uditgulati6@gmail.com>
2024-06-02 20:42:21 +0530

    remove incorrect/unused comments in contract neutron_slinky

--- contracts/neutron_slinky/src/msg.rs
+++ contracts/neutron_slinky/src/msg.rs
@@ -6,13 +6,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    /// this query goes to neutron and get stored ICA with a specific query
     GetPrice {
         base_symbol: String,
         quote_currency: String,
         max_blocks_old: Uint64,
     },
-    // this query returns ICA from contract store, which saved from acknowledgement
     GetPrices {
         base_symbols: Vec<String>,
         quote_currency: String,

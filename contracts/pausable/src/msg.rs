8b625979b920a57348f9018702f897af5d748dd4
Sergey Ratiashvili <serg.s.r@gmail.com>
2024-07-10 14:18:25 +0200

    feat: pausable mock

---
+++ contracts/pausable/src/msg.rs
@@ -0,0 +1,18 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Pause {},
+    Unpause {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    State {},
+}
